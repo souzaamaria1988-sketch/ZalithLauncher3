e7b406c0be82ef67e36323184a7d4d8673e5d665
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-09-01 17:10:00 -0300

    fase0: atualiza settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,10 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
-}
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -23,17 +12,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
-rootProject.name = "ZalithLauncher"
-include(":ZalithLauncher")
-include(":LWJGL")
-include(":LWJGL:patches")
-project(":LWJGL:patches").projectDir = file("LWJGL/patches")
-include(":LWJGL:lwjgl-3.3.3")
-project(":LWJGL:lwjgl-3.3.3").projectDir = file("LWJGL/3.3.3")
-include(":LWJGL:lwjgl-3.4.1")
-project(":LWJGL:lwjgl-3.4.1").projectDir = file("LWJGL/3.4.1")
-include(":LayerController")
-include(":ColorPicker")
-include(":Terracotta")
-include(":InputMap")
+rootProject.name = "ZalithLauncher3"
+include(":app", ":core")
