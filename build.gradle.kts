b9f9b4e051cd3a6be102eb4f19ae4e24aaf09758
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-09-01 17:10:02 -0300

    fase0: atualiza build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.ksp.plugin) apply false
-    kotlin("plugin.serialization") version libs.versions.kotlin apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.hilt) apply false
-}
-
-buildscript {
-    repositories {
-        maven { url = uri("https://jitpack.io") }
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath(libs.buildkeys)
-    }
+    id("com.android.application") version "8.5.2" apply false
+    id("com.android.library") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
