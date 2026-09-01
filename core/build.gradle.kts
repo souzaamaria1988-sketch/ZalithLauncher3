c7badf7f664db21ec772a9072327347a8bfc0b2c
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-09-01 17:10:10 -0300

    fase0: adiciona core/build.gradle.kts

---
+++ core/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.zalith.launcher.core"
+    compileSdk = 34
+    defaultConfig { minSdk = 26 }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions { jvmTarget = "17" }
+}
